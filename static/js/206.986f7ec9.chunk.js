08f8915604d419009c4a193b0882714f03083064
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2024-02-28 11:25:20 +0000

    Deploying to gh-pages from @ Olesia511/project-app2024@e1888a5be412ebc98789665da97e4a713497d289 🚀

---
+++ static/js/206.986f7ec9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{206:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("h1",{children:"HOME PAGE"})}}}]);
+//# sourceMappingURL=206.986f7ec9.chunk.js.map
